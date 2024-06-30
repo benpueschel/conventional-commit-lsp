fa5dcf04f16035f8e8748bd9eb7bad74af37087d
Ben Püschel <ben.pueschel2004@gmail.com>
2024-06-30 23:59:32 +0200

    feat: check for correct commit type, scope and !

--- analysis/diagnostics.go
+++ analysis/diagnostics.go
@@ -2,6 +2,7 @@ package analysis
 
 import (
 	"strings"
+	"unicode"
 
 	"github.com/benpueschel/conventional-commit-lsp/lsp"
 )
@@ -55,16 +56,7 @@ func getHeaderDiagnostics(header string) []lsp.Diagnostic {
 		return diagnostics
 	}
 
-	if strings.Contains(commit_type, " ") {
-		line_range := LineRange(0, 0, 0, len(commit_type))
-		diagnostics = append(diagnostics, lsp.Diagnostic{
-			Severity: 1,
-			Range:    line_range,
-			Source:   "conventional-commit-lsp",
-			Message:  "Commit type must not contain spaces",
-		})
-		return diagnostics
-	}
+	diagnostics = append(diagnostics, getTypeDiagnostic(commit_type)...)
 
 	if strings.TrimSpace(description) == "" {
 		line_range := LineRange(0, len(commit_type)+1, 0, len(header))
@@ -85,7 +77,96 @@ func getHeaderDiagnostics(header string) []lsp.Diagnostic {
 			Message:  "Commit message must contain a type",
 		})
 	}
-	_ = description
 
 	return diagnostics
+}
+
+func getTypeDiagnostic(commit_type string) []lsp.Diagnostic {
+	diagnostics := []lsp.Diagnostic{}
+	if strings.Contains(commit_type, " ") {
+		line_range := LineRange(0, 0, 0, len(commit_type))
+		diagnostics = append(diagnostics, lsp.Diagnostic{
+			Severity: 1,
+			Range:    line_range,
+			Source:   "conventional-commit-lsp",
+			Message:  "Commit type must not contain spaces",
+		})
+	}
+
+	commit_type, scope, found := strings.Cut(commit_type, "(")
+	if !found {
+		commit_type, breaking, _ := strings.Cut(commit_type, "!")
+		idx := len(commit_type) + len(scope) + 1
+		diagnostics := checkBreakingHeaderDiagnostic(breaking, idx, diagnostics)
+		if !isAlphabetic(commit_type) {
+			line_range := LineRange(0, 0, 0, len(commit_type))
+			diagnostics = append(diagnostics, lsp.Diagnostic{
+				Severity: 1,
+				Range:    line_range,
+				Source:   "conventional-commit-lsp",
+				Message:  "Commit type must be alphabetic",
+			})
+		}
+		return diagnostics
+	}
+
+	scope, breaking, found := strings.Cut(scope, ")")
+	if !found {
+		idx := len(commit_type) + len(scope) + 1
+		line_range := LineRange(0, idx, 0, idx+1)
+		diagnostics = append(diagnostics, lsp.Diagnostic{
+			Severity: 1,
+			Range:    line_range,
+			Source:   "conventional-commit-lsp",
+			Message:  "Unclosed scope. Insert a closing parenthesis",
+		})
+	}
+
+	idx := len(commit_type) + len(scope) + 2
+	diagnostics = checkBreakingHeaderDiagnostic(breaking, idx, diagnostics)
+
+	if !isAlphabetic(commit_type) {
+		line_range := LineRange(0, 0, 0, len(commit_type))
+		diagnostics = append(diagnostics, lsp.Diagnostic{
+			Severity: 1,
+			Range:    line_range,
+			Source:   "conventional-commit-lsp",
+			Message:  "Commit type must be alphabetic",
+		})
+	}
+
+	if !isAlphabetic(scope) {
+		idx := len(commit_type) + len(scope) + 1
+		line_range := LineRange(0, len(commit_type)+1, 0, idx)
+		diagnostics = append(diagnostics, lsp.Diagnostic{
+			Severity: 1,
+			Range:    line_range,
+			Source:   "conventional-commit-lsp",
+			Message:  "Scope must be alphabetic",
+		})
+	}
+
+	return diagnostics
+}
+
+func checkBreakingHeaderDiagnostic(breaking string, idx int, diagnostics []lsp.Diagnostic) []lsp.Diagnostic {
+	if breaking != "" && breaking != "!" {
+		line_range := LineRange(0, idx, 0, idx+len(breaking))
+		diagnostics = append(diagnostics, lsp.Diagnostic{
+			Severity: 1,
+			Range:    line_range,
+			Source:   "conventional-commit-lsp",
+			Message:  "Breaking change indicator must be '!'",
+		})
+	}
+	return diagnostics
+}
+
+func isAlphabetic(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
 }
